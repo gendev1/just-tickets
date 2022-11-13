81a6af6633cf828ab192e9d3353bebf4d48d61fd
generalist dev <hello@generalist.dev>
2022-11-12 21:52:13 -0500

    basic ticket-details setup

---
+++ client/src/pages/ticketDetails/index.js
@@ -0,0 +1,10 @@
+function TicketDetails() {
+  return (
+    <div className="ticket-details-container">
+      <section className="section">
+        <div className="container"></div>
+      </section>
+    </div>
+  );
+}
+export default TicketDetails;
